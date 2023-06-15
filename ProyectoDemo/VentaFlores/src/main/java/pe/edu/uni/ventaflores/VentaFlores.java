06a91e79b7a94bd8808189cb1683829f8d8f84c9
Gustavo Coronel <gcoronelc@gmail.com>
2023-06-15 16:12:46 -0500

    Semana 11

---
+++ ProyectoDemo/VentaFlores/src/main/java/pe/edu/uni/ventaflores/VentaFlores.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package pe.edu.uni.ventaflores;
+
+/**
+ *
+ * @author LAB M5-E
+ */
+public class VentaFlores {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
